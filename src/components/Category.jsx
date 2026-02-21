05f836abe3eae1fba3c47e9c2c3fde246b44d467
juwel-001 <juwelice5683@gmail.com>
2026-02-21 23:47:31 +0500

    changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -17,19 +17,18 @@ function Category() {
     
 
   return (
-    <div className='bg-[#13203a]'>
-  <div className='max-w-7xl mx-auto grid grid-cols-5 gap-6 py-12 px-2 md:justify-around justify-center'>
+   <div className='bg-[#13203a]'>
+  <div className='max-w-7xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 py-12 px-2 justify-center'>
     {
-      categoryOnlyData ?.map((item, index) => {
-        return (
-          <button onClick={()=>navigate(`/category/${item}`)} 
-            key={index} 
-            className='bg-linear-to-r from-red-500 to-purple-500 text-white md:px-3 px-4 py-2 uppercase text-center rounded-md cursor-pointer overflow-x-hidden'
-          >
-            {item}
-          </button>
-        );
-      })
+      categoryOnlyData?.map((item, index) => (
+        <button 
+          onClick={()=>navigate(`/category/${item}`)} 
+          key={index} 
+          className='bg-linear-to-r from-red-500 to-purple-500 text-white px-2 sm:px-3 md:px-4 py-2 uppercase text-center rounded-md cursor-pointer whitespace-normal break-words'
+        >
+          {item}
+        </button>
+      ))
     }
   </div>
 </div>
